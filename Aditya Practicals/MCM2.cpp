b19db20e903d3e5bfce27a07c9d8dc4e676e4483
Aditya <adityavishal903@gmail.com>
2023-04-13 15:17:46 +0530

    .

--- Aditya Practicals/MCM2.cpp
+++ Aditya Practicals/MCM2.cpp
@@ -1,52 +1,50 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void print_tables(int (*table)[], int n, int size) {
+void print_tables(int table[][100], int n) {
     for(int i=1; i<=n; i++) {
         for(int j=1; j<=n; j++) {
-            cout << setw(10) << table[size*(i-1)+(j-1)] << " ";
+            cout << setw(10) << table[i][j] << " ";
         }
         cout << endl;
     }
 }
 
-void print_optimal_parens(int (*s)[], int i, int j) {
+void print_optimal_parentheses(int s[][100], int i, int j) {
     if(i == j) {
         cout << "A" << i;
     } else {
         cout << "(";
-        print_optimal_parens(s, i, s[(i-1)*n+(j-1)]);
-        print_optimal_parens(s, s[(i-1)*n+(j-1)]+1, j);
+        print_optimal_parentheses(s, i, s[i][j]);
+        print_optimal_parentheses(s, s[i][j]+1, j);
         cout << ")";
     }
 }
 
 int matrix_chain_order(int p[], int n) {
-    int m[n*n], s[(n-1)*(n-1)];
-    for(int i=0; i<n*n; i++) m[i] = 0;
-    for(int i=0; i<(n-1)*(n-1); i++) s[i] = 0;
-    for(int i=1; i<n; i++) m[(n+1)*i+i] = 0;
+    int m[100][100], s[100][100];
+    for(int i=1; i<n; i++) m[i][i] = 0;
     for(int L=2; L<n; L++) {
-        for(int i=1; i<=n-L+1; i++) {
+        for(int i=1; i<n-L+1; i++) {
             int j = i+L-1;
-            m[(n+1)*i+j] = INT_MAX;
-            for(int k=i; k<j; k++) {
-                int q = m[(n+1)*i+k] + m[(n+1)*(k+1)+j] + p[i-1]*p[k]*p[j];
-                if(q < m[(n+1)*i+j]) {
-                    m[(n+1)*i+j] = q;
-                    s[(n-1)*(i-1)+(j-2)] = k;
+            m[i][j] = INT_MAX;
+            for(int k=i; k<=j-1; k++) {
+                int q = m[i][k] + m[k+1][j] + p[i-1]*p[k]*p[j];
+                if(q < m[i][j]) {
+                    m[i][j] = q;
+                    s[i][j] = k;
                 }
             }
         }
     }
     cout << "m table:\n";
-    print_tables(m, n, n+1);
+    print_tables(m, n-1);
     cout << "\ns table:\n";
-    print_tables(s, n-1, n-1);
-    cout << "\noptimal parenthesization: ";
-    print_optimal_parens(s, 1, n-1);
+    print_tables(s, n-1);
+    cout << "\nOptimal Parentheses: ";
+    print_optimal_parentheses(s, 1, n-1);
     cout << endl;
-    return m[n+1];
+    return m[1][n-1];
 }
 
 int main() {
